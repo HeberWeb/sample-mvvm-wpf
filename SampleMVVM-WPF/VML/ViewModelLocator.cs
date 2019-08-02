8a2318902ce76844c6ae726ffcd86e3ba74295f5
Heber Pietrafeza <hpietrafeza@autodoc.com.br>
2019-08-02 12:51:42 -0300

    implement folders, files defaults, and View First Construction in XAML, View First Construction in Code-behind and ViewModelLocator(Hooking Up ViewModel)

---
+++ SampleMVVM-WPF/VML/ViewModelLocator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SampleMVVM_WPF.VML
+{
+    public static class ViewModelLocator
+    {
+        public static bool GetAutoHookedUpViewModel(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(AutoHookedUpViewModelProperty);
+        }
+
+        public static void SetAutoHookedUpViewModel(DependencyObject obj, bool value)
+        {
+            obj.SetValue(AutoHookedUpViewModelProperty, value);
+        }
+
+        public static readonly DependencyProperty AutoHookedUpViewModelProperty =
+            DependencyProperty.RegisterAttached("AutoHookedUpViewModel",
+                typeof(bool), typeof(ViewModelLocator), new PropertyMetadata(false, AutoHookedUpViewModelChanged));
+
+        private static void AutoHookedUpViewModelChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (DesignerProperties.GetIsInDesignMode(d)) return;
+            var viewType = d.GetType();
+
+            string strReplace = viewType.FullName;
+            strReplace = strReplace.Replace(".Views.", ".ViewModel.");
+
+            var viewTypeName = strReplace;
+            var viewModelTypeName = viewTypeName + "Model";
+            var viewModelType = Type.GetType(viewModelTypeName);
+            var viewModel = Activator.CreateInstance(viewModelType);
+
+            ((FrameworkElement)d).DataContext = viewModel;
+        }
+    }
+}
---
+++ SampleMVVM-WPF/ViewModel/StudentViewModel.cs
@@ -0,0 +1,26 @@
+﻿using SampleMVVM_WPF.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleMVVM_WPF.ViewModel
+{
+    class StudentViewModel
+    {
+        public StudentViewModel()
+        {
+            LoadStudents();
+        }
+        public ObservableCollection<Student> Students { get; set; } = new ObservableCollection<Student>();
+
+        public void LoadStudents()
+        {
+            Students.Add(new Student { FirstName = "Diego", LastName = "Rondão" });
+            Students.Add(new Student { FirstName = "Diogo", LastName = "Rondão" });
+            Students.Add(new Student { FirstName = "João", LastName = "Victor" });
+        }
+    }
+}
