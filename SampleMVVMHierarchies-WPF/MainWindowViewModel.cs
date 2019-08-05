da44df141b5c5065906968e28f414d52f049231b
Heber Pietrafeza <hpietrafeza@autodoc.com.br>
2019-08-05 13:21:46 -0300

    verificando hierarchies

---
+++ SampleMVVMHierarchies-WPF/MainWindowViewModel.cs
@@ -0,0 +1,48 @@
+﻿using SampleMVVMHierarchies_WPF.ViewModel;
+using SampleMVVMHierarchies_WPF.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleMVVMHierarchies_WPF
+{
+    class MainWindowViewModel : BindableBase
+    {
+
+        public MainWindowViewModel()
+        {
+            NavCommand = new MyICommand<string>(OnNav);
+        }
+
+        private CustomerListViewModel custListViewModel = new CustomerListViewModel();
+
+        private OrderViewModel orderViewModelModel = new OrderViewModel();
+
+        private BindableBase _CurrentViewModel;
+
+        public BindableBase CurrentViewModel
+        {
+            get { return _CurrentViewModel; }
+            set { SetProperty(ref _CurrentViewModel, value); }
+        }
+
+        public MyICommand<string> NavCommand { get; private set; }
+
+        private void OnNav(string destination)
+        {
+
+            switch (destination)
+            {
+                case "orders":
+                    //CurrentViewModel = orderViewModelModel;
+                    break;
+                case "customers":
+                default:
+                    //CurrentViewModel = custListViewModel;
+                    break;
+            }
+        }
+    }
+}
---
+++ SampleMVVMHierarchies-WPF/MyICommand.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace SampleMVVMHierarchies_WPF
+{
+    public class MyICommand<T> : ICommand
+    {
+        Action<T> _TargetExecuteMethod;
+        Func<T, bool> _TargetCanExecuteMethod;
+
+        public MyICommand(Action<T> executeMethod)
+        {
+            _TargetExecuteMethod = executeMethod;
+        }
+
+        public MyICommand(Action<T> executeMethod, Func<T, bool> canExecuteMethod)
+        {
+            _TargetExecuteMethod = executeMethod;
+            _TargetCanExecuteMethod = canExecuteMethod;
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged(this, EventArgs.Empty);
+        }
+
+        #region ICommand Members
+
+        // Beware - should use weak references if command instance lifetime 
+        //is longer than lifetime of UI objects that get hooked up to command
+
+        // Prism commands solve this in their implementation 
+        public event EventHandler CanExecuteChanged = delegate { };
+
+        bool ICommand.CanExecute(object parameter)
+        {
+            if (_TargetCanExecuteMethod != null)
+            {
+                T tparm = (T)parameter;
+                return _TargetCanExecuteMethod(tparm);
+            }
+
+            if (_TargetExecuteMethod != null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        void ICommand.Execute(object parameter)
+        {
+            if (_TargetExecuteMethod != null)
+            {
+                _TargetExecuteMethod((T)parameter);
+            }
+        }
+        #endregion
+    }
+}
