321ead4853fc0ab7114d0d37eaba5b8c983cfd05
Heber Pietrafeza <hpietrafeza@autodoc.com.br>
2019-08-06 14:37:10 -0300

    init dependency injection

---
+++ SampleMVVMHierarchies-WPF/Services/CustomersRepository.cs
@@ -0,0 +1,17 @@
+﻿using SampleMVVMHierarchies_WPF.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleMVVMHierarchies_WPF.Services
+{
+    //public class CustomersRepository : ICustomersRepository<Customer>
+    //{
+    //    public Task<List<Customer>> GetCustomersAsync()
+    //    {
+    //        return _context.Customers.ToListAsync();
+    //    }
+    //}
+}
---
+++ SampleMVVMHierarchies-WPF/Services/ICustomersRepository.cs
@@ -0,0 +1,19 @@
+﻿using SampleMVVMHierarchies_WPF.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleMVVMHierarchies_WPF.Services
+{
+    public interface ICustomersRepository<T>
+    {
+        Task<List<T>> GetCustomersAsync();
+        Task<T> GetCustomerAsync(Guid id);
+        Task<T> AddCustomerAsync(T customer);
+        Task<T> UpdateCustomerAsync(T customer);
+        Task DeleteCustomerAsync(Guid customerId);
+    }
+
+}
